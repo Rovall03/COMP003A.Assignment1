1c048e7c6a66681ad51bd14d84471bd87fa506b3
Rovall03 <rosaovalle@my.whccd.edu>
2025-01-15 20:49:51 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter your name: ");
+            string username = Console.ReadLine();//gets the user input and saves it to a variable named username.
+
+            Console.WriteLine("Hi " + username + ". My name is Rosa.");
+           
+            Console.Write("10 facts ");
+
+
+        }
+    }
+}
