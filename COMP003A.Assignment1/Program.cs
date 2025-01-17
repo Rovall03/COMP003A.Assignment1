e2f9fa174c2b6621a9aeb42de102c3b2598be9c9
rosa ovalle <you@example.com>
2025-01-16 18:00:34 -0800

    added 10 thongs

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -8,9 +8,19 @@
             Console.Write("Enter your name: ");
             string username = Console.ReadLine();//gets the user input and saves it to a variable named username.
 
-            Console.WriteLine("Hi " + username + ". My name is Rosa.");
+            Console.WriteLine("Hey " + username + "!. My name is Rosa!");
            
-            Console.Write("10 facts ");
+            Console.Write("\n Here are 10 thiongs about me:" +
+                "\n 1) I am a mother 3 " +
+                "\n 2) I have 1 cat" +
+                "\n 3) I enjoy weight lifting" +
+                "\n 4) I hate arm day" +
+                "\n 5) I make all my own bread" +
+                "\n 6) I love to travel" +
+                "\n 7) I hate pudding" +
+                "\n 8) My major is in IT" +
+                " \n 9) I love music" +
+                " \n 10) Concerts are my favorite pass time");
 
 
         }
